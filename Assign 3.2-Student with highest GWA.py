aabd210f069d71a79111afdc14d17572cb4a8eca
rsevial <rebekahsevial@gmail.com>
2023-04-25 20:20:48 +0800

    Initialize student name and their highest possible GWA

--- Assign 3.2-Student with highest GWA.py
+++ Assign 3.2-Student with highest GWA.py
@@ -5,6 +5,8 @@
 # Open text file that contains the name of the students with their GWA
 with open("students_record.txt") as student_GWA:
 # Initialize student name and their highest possible GWA 
+    highest_GWA = 1.00
+    student_name = ""
 # For loop to read each line of the text file
 # Function to split the name and GWA
 # If statement to get the name of student who got the highest GWA
