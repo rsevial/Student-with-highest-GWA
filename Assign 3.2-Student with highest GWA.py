3e2fb6996b92b34414a5d69e2e9a68ecc1327354
rsevial <rebekahsevial@gmail.com>
2023-04-25 20:31:05 +0800

    Proper indentation

--- Assign 3.2-Student with highest GWA.py
+++ Assign 3.2-Student with highest GWA.py
@@ -4,14 +4,14 @@
 
 # Open text file that contains the name of the students with their GWA
 with open("students_record.txt") as student_GWA:
-# Initialize student name and their lowest possible GWA 
+    # Initialize student name and their lowest possible GWA 
     highest_GWA = 5.00
     student_name = ""
-# For loop to read each line of the text file
+    # For loop to read each line of the text file
     for line in student_GWA:
-# Function to split the name and GWA
+        # Function to split the name and GWA
         name, GWA_str = line.strip().split(" , ")
-# If statement to get the name of student who got the highest GWA
+        # If statement to get the name of student who got the highest GWA
         GWA = float(GWA_str)
         if GWA < highest_GWA:
             highest_GWA = GWA
