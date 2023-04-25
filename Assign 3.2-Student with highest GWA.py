fd03243d1730ec50314c17430f6e5a7566c92ab0
rsevial <rebekahsevial@gmail.com>
2023-04-25 19:57:03 +0800

    Create Pseudocode

---
+++ Assign 3.2-Student with highest GWA.py
@@ -0,0 +1,10 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Problem 2 of Assignment 3
+# Student with highest GWA
+
+# Open text file that contains the name of the students with their GWA
+# Initialize student name and their highest possible GWA 
+# For loop to read each line of the text file
+# Function to split the name and GWA
+# If statement to get the name of student who got the highest GWA
+# Print the name of the student with highest GWA and thier GWA
